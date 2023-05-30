981740489916ab70a3d7f770bc4b33efa504a3c9
GaltNOR <128933966+GaltNOR@users.noreply.github.com>
2023-05-30 19:35:23 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,17 +3,9 @@ import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
-}
+return (
+  <main>
+    <Title>Hello World</Title>
+    <iframe src="www.vg.no" width="100%" height="500px"></iframe>
+  </main>
+);
